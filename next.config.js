a9544def5b067c916d52fde9742cc2cd042d7bbc
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2021-06-27 18:47:12 -0400

    Configuracion de variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants');
+
+module.exports = phase => {
+  const isDevelopment = phase === PHASE_DEVELOPMENT_SERVER
+  
+  const env = {
+    SITE_NAME: "Dagmar Blog",
+    API_BLOG: (()=>{
+      if(isDevelopment){
+        return "https://jsonplaceholder.typicode.com/"
+         } else {
+           return "https://api.dagmarblog.com"
+         }
+    })()
+  }
+
+  return {
+    env,
+    reactStrictMode: true
+  }
 }
